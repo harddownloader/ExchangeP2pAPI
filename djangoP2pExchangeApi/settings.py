3b51089c488c21ff797a45b8e832b5326aa9b0b8
Serafim Krutskevich <55257833+harddownloader@users.noreply.github.com>
2024-06-13 15:22:19 +0300

    updated settings.py: added MEDIA_URL and MEDIA_ROOT variables

--- djangoP2pExchangeApi/settings.py
+++ djangoP2pExchangeApi/settings.py
@@ -143,7 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
